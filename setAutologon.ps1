81cafdf1c541f079c459badaf3681d8c8df08eb5
Ajit Abhipallav <ajit.abhipallav@ni.com>
2016-06-03 00:48:53 +0530

    rename computer scripts to ps1 file

--- setAutologon.ps1
+++ setAutologon.ps1
@@ -2,5 +2,18 @@ Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlo
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\" -Name DefaultPassword -value "Labview==="
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\" -Name DefaultUserName -value "labviewbuild"
 
+# Get the IPv4 address of Azure Machine ( Internal IP )
+$vmName = (Get-ItemProperty –path “HKLM:\SOFTWARE\Microsoft\Virtual Machine\Auto”).RDPAddressIPv4
+# Replace any non-alphanumeric characters with an underscore
+$vmName = [Regex]::Replace($vmName,"\W","-")
+
+           
+Write-Host "Renaming the Computer and Restarting....."
+# Trim names that are longer than 15 characters
+$vmName = ("AZ" + $vmName.Substring($vmName.Length - [System.Math]::Min(13, $vmName.Length),[System.Math]::Min(13, $vmName.Length)));
+ 
+# Check the trimmed and cleaned VM name against the guest OS name
+# If it is different, change the guest OS name and reboot
+if ($env:computername -ne $vmName) {(gwmi win32_computersystem).Rename($vmName) }
 
 shutdown -r -f -t 30
