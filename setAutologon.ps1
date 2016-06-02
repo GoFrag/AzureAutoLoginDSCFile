0db819ac74e5fbfb075503c95c5713b98655d92f
Ajit Abhipallav <ajit.abhipallav@ni.com>
2016-06-03 00:13:50 +0530

    ps1 file

---
+++ setAutologon.ps1
@@ -0,0 +1,6 @@
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\" -Name AutoAdminLogon -value 1
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\" -Name DefaultPassword -value "Labview==="
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\" -Name DefaultUserName -value "labviewbuild"
+
+
+shutdown -r -f -t 120
