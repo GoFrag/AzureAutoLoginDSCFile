91d3c37e0129775615786938ea3e43ef82cad402
Ajit Abhipallav <ajit.abhipallav@ni.com>
2016-06-28 12:42:02 +0530

    Renaming in HEX representation.

--- setAutologonDecimalIP.ps1
+++ setAutologonDecimalIP.ps1
@@ -3,19 +3,14 @@ Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlo
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\" -Name DefaultUserName -value "labviewbuild"
 
 # Get the IPv4 address of Azure Machine ( Internal IP )
-$vmPrivateIP = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Auto").RDPAddressIPv4
+$vmName = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Auto").RDPAddressIPv4
+# Replace any non-alphanumeric characters with an underscore
+$vmName = [Regex]::Replace($vmName,"\W","-")
 
-# Get HEX representation from IP for setting computer name
-$ipParts = $vmPrivateIP.Split('.')
-$part1 = "{0:X2}" -f [int]$ipParts[0]
-$part2 = "{0:X2}" -f [int]$ipParts[1]
-$part3 = "{0:X2}" -f [int]$ipParts[2]
-$part4 = "{0:X2}" -f [int]$ipParts[3]
-$vmName = $part1 + $part2 + $part3 + $part4
            
 Write-Host "Renaming the Computer and Restarting....."
 # Trim names that are longer than 15 characters
-$vmName = ("AZ-" + $vmName.Substring($vmName.Length - [System.Math]::Min(13, $vmName.Length),[System.Math]::Min(13, $vmName.Length)));
+$vmName = ("AZ" + $vmName.Substring($vmName.Length - [System.Math]::Min(13, $vmName.Length),[System.Math]::Min(13, $vmName.Length)));
  
 # Check the trimmed and cleaned VM name against the guest OS name
 # If it is different, change the guest OS name and reboot
